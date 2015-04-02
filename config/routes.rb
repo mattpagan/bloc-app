f254aeee21e1faeb8e158660bf25b2f6d06beaf8
Matt Pagan <matthew.pagan@me.com>
2015-04-01 23:00:18 -0400

    Complete crud assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
  resources :posts
+ resources :advertisements
 
   get 'welcome/contact'
   get 'about' => 'welcome#about'
+  get 'advertisements' => 'advertisements#index'
     
     root to: 'welcome#index'
 end
