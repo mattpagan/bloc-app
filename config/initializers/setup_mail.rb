416edfa964b55f6c2a23febd4d7fd8127a226d5c
Matt Pagan <matthew.pagan@me.com>
2015-05-06 19:27:29 -0400

    Configured app for Production emails

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,6 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
+if Rails.env.development? || Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
@@ -8,3 +9,4 @@ ActionMailer::Base.smtp_settings = {
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
+end
