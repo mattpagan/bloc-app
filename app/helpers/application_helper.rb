548a7e56294bfbe7fe219e1c6f56b3b7904c3daf
Matt Pagan <matthew.pagan@me.com>
2015-06-01 17:04:22 -0400

    Assignment 42 Validating Posts Reworked

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,12 +6,12 @@ module ApplicationHelper
      else
        content_tag :div, capture(&block), class: 'form-group'
      end
-   	end
+ 	end
 
-   	def markdown_to_html(markdown)
-  renderer = Redcarpet::Render::HTML.new
-  extensions = {fenced_code_blocks: true}
-  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-  (redcarpet.render markdown).html_safe
-	end
+  def markdown_to_html(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
 end
