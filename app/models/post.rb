964112ba8541d7e2e9014e40bd7299256b5b933d
Matt Pagan <matthew.pagan@me.com>
2015-07-20 04:24:25 -0400

    Complete voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,15 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :topic
 
+	default_scope { order('rank DESC') }
+
+    validates :title, length: { minimum: 5 }, presence: true
+    validates :body, length: { minimum: 20 }, presence: true
+    # validates :topic, presence: true
+    # validates :user, presence: true
+ 
+    after_create :create_vote
+
 	def up_votes
 		votes.where(value: 1).count
 	end
@@ -23,10 +32,9 @@ class Post < ActiveRecord::Base
      	update_attribute(:rank, new_rank)
    	end
 	
-	default_scope { order('rank DESC') }
+	private
 
-   	validates :title, length: { minimum: 5 }, presence: true
-   	validates :body, length: { minimum: 20 }, presence: true
-    # validates :topic, presence: true
-    # validates :user, presence: true
+	def create_vote
+		user.votes.create(value: 1, post: self)
+	end
 end
