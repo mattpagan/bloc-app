5d3c7147bc02ab11d1e7c97f3d527458d9fdcb51
Matt Pagan <matthew.pagan@me.com>
2015-07-22 21:29:45 -0400

    Reached Testing Action Mailer Section in Checkpoint 51 Favoriting

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,30 @@
+class FavoritesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: @post)
+    
+    authorize favorite
+    if favorite.save
+      flash[:notice] = "Favorited post"
+      redirect_to [@post.topic, @post]
+    else
+      #Add code to generate a failure flash and redirect to post
+      flash[:error] = "There was an error, favoriting the post"
+      redirect_to [@post.topic, @post]
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+
+    authorize favorite
+    if favorite.destroy
+      flash[:notice] = "Removed favorite."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "Unable to remove favorite. Please try again."
+      redirect_to [@post.topic, @post]
+    end
+  end
+end
