865a4a3d0a1c122e0b6fd630a8632d442c1b6898
Matt Pagan <matthew.pagan@me.com>
2015-07-26 16:30:55 -0400

    Update Assignment 52 completed, Private Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
 	has_many :posts, dependent: :destroy
 
 	scope :visible_to, -> (user) { user ? all : where(public: true) }
-end
+	scope :publicly_viewable, -> { where(public: true) }
+	scope :privately_viewable, -> { where(public: false) }
+ end
