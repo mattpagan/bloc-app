c030071c3c5c1e82ced78aebc4907ed53d2146df
Matt Pagan <matthew.pagan@me.com>
2015-08-14 05:44:32 -0400

    Checkpoint 58 complete, Creating with Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,39 +1,48 @@
 class CommentsController < ApplicationController
 
-   def create
-     @post = Post.find(params[:post_id])
-     @comment = @post.comments.new(comment_params)
-     @comment.user_id = current_user.id
-     
-     if @comment.save 
-       redirect_to [@post.topic, @post], notice: "Comment saved successfully"
-     else
-       flash[:error] = "There was an error when trying to save your comment, please try again."
-       redirect_to [@post.topic, @post]
-     end
-   end
+def create
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
 
-   def destroy
-     @post = Post.find(params[:post_id])
-     @comment = @post.comments.find(params[:id])
-     authorize @comment
-     
-     if @comment.destroy
-       flash[:notice] = "Comment was removed."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:error] = "Comment couldn't be deleted. Try again."
-     end
+    @comment = Comment.new( comment_params )
+    @comment.user = current_user
+    @comment.post = @post
+    @new_comment = Comment.new
 
-     respond_to do |format|
+    authorize @comment
+
+    if @comment.save
+      flash[:notice] = "Comment was created."
+    else
+      flash[:error] = "There was an error saving the comment. Please try again."
+    end
+
+    respond_to do |format|
       format.html
       format.js
-     end
+    end
+  end
+
+def destroy
+   @post = Post.find(params[:post_id])
+   @comment = @post.comments.find(params[:id])
+   authorize @comment
+   
+   if @comment.destroy
+     flash[:notice] = "Comment was removed."
+   else
+     flash[:error] = "Comment couldn't be deleted. Try again."
    end
- 
-   private
- 
-   def comment_params
-     params.require(:comment).permit(:body)
-   end
+
+   respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
